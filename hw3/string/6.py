54eac420698500ff41c01ecb1d7b7fe9b8bf6d9b
aabniaz <a_abobakir@kbtu.kz>
2023-09-25 21:16:37 +0600

    hw3

--- hw3/string/6.py
+++ hw3/string/6.py
@@ -1,10 +1,11 @@
 a = input()
 try:
-    if a[-3:] != 'ing':
-        print(a + 'ing')
-
-except:
-    print('length is less than 3')
+    if len(a) < 3:
+        raise ValueError('length is less than 3')
+except ValueError as e:
+    print('error:', str(e))
 else:
     if a[-3:] == 'ing':
         print(a + 'ly')
+    else:
+        print(a + 'ing')
